b43f0148fa115e2ce917cb82c8df748648df0865
sayali1098 <sauamritkar@gmail.com>
2020-07-30 19:40:46 +0530

    FSD0620B102

---
+++ directive-ngswitch-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+// import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
